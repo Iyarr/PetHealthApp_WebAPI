bf98275b409fe72cc5aae3a67ae1c3fab44957af
Iyarr <8.24ridiculous@gmail.com>
2024-10-16 13:34:16 +0900

    #26 テスト用コンテナによるテスト実行成功

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,36 @@
-import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
+import {
+  CreateTableCommand,
+  ListTablesCommand,
+  CreateTableCommandInput,
+} from "@aws-sdk/client-dynamodb";
 import { DBClient } from "./utils/dynamodb.js";
 import { getEnv } from "./utils/env.js";
 
 const TABLE_PREFIX = getEnv("TABLE_PREFIX");
 
-const createDogsTableCommand = new CreateTableCommand({
+class CreateTableCommands {
+  commands: CreateTableCommand[];
+  constructor() {
+    this.commands = [];
+  }
+
+  add(command: CreateTableCommandInput) {
+    this.commands.push(new CreateTableCommand(command));
+  }
+
+  async create() {
+    const listTablesCommandResult = await DBClient.send(new ListTablesCommand({}));
+    console.log(listTablesCommandResult);
+    for (const command of this.commands) {
+      if (!listTablesCommandResult.TableNames.includes(command.input.TableName)) {
+        console.log(await DBClient.send(command));
+      }
+    }
+  }
+}
+
+const createTableCommands = new CreateTableCommands();
+createTableCommands.add({
   AttributeDefinitions: [
     {
       AttributeName: "id",
@@ -39,7 +65,7 @@ const createDogsTableCommand = new CreateTableCommand({
   BillingMode: "PAY_PER_REQUEST",
 });
 
-const createUsersTableCommand = new CreateTableCommand({
+createTableCommands.add({
   AttributeDefinitions: [
     {
       AttributeName: "uid",
@@ -55,3 +81,5 @@ const createUsersTableCommand = new CreateTableCommand({
   TableName: `${TABLE_PREFIX}Users`,
   BillingMode: "PAY_PER_REQUEST",
 });
+
+await createTableCommands.create();
