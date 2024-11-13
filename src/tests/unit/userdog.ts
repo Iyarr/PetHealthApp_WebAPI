320876dd1f9802abb2910c034f28e211110df891
Iyarr <8.24ridiculous@gmail.com>
2024-11-13 09:54:43 +0900

    #54 テストデータの作成

--- src/tests/unit/userdog.ts
+++ src/tests/unit/userdog.ts
@@ -1,28 +1,35 @@
 import { test } from "node:test";
 import { strict } from "node:assert";
+import { randomUUID } from "node:crypto";
 import { userDogModel } from "../../models/userdog.js";
 import { dogModel } from "../../models/dog.js";
 import { UserDogPOSTRequestBody } from "../../types/userdog.js";
 import { DogPOSTRequestBody } from "../../types/dog.js";
+
+const userNum = 9;
+const dogNum = 12;
+const userDogSpecies = 3;
 
 const sizes = {
   0: "small",
   1: "medium",
   2: "big",
 };
-const testDogItems: DogPOSTRequestBody[] = Array(10).map((i: number) => {
-  return {
-    id: `testId${i}`,
+const testUids = Array(userNum).map((i: number) => `testUid${i}`);
+
+const testDogItems = Array(dogNum).map((i: number) => {
+  const reqBody: DogPOSTRequestBody = {
     name: `testName${i}`,
     gender: i % 2 ? "male" : "female",
     size: sizes[i % 3],
   };
+  const id = randomUUID();
+  const hostUid = testUids[i % userNum];
+  return {
+    id,
+    ...reqBody,
+  };
 });
-const testUids = Array(3).map((i: number) => `testUid${i}`);
-// ユーザーごとにどの犬のアクセス権限を持たせるかを決める
-// 0: 小さい犬のアクセス権限を持つ
-// 1: 中くらいの犬のアクセス権限を持つ
-// 2: 大きい犬のアクセス権限を持つ
 
 const testUserDogItems: UserDogPOSTRequestBody[] = testDogItems.map((testDogItem) => {
   if (testDogItem.size === "small") {
